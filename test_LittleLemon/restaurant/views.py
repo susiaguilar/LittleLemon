9a54f2afa74afcc9a43a96db7d642708cab18fad
Susan <susanjoaguilar@gmail.com>
2023-02-27 00:44:06 -0600

    set up API

--- test_LittleLemon/restaurant/views.py
+++ test_LittleLemon/restaurant/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics,viewsets, permissions
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import userSerializer,menuSerializer,bookingSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+#class bookingView(APIView):
+ #   def get(self, request):
+  #      items = Booking.objects.all()
+   #     serializer = bookingSerializer(items, many=True)
+    #    return Response(seralizer.data) #return JSON
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+
+    def post(self, request):
+        serializer = menuSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data" : "serializer.data"})
+
+class menuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+    #permission_classes = [permissions.IsAuthenticated] 
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+    #permission_classes = [permissions.IsAuthenticated] 
--- test_LittleLemon/test_LittleLemon/urls.py
+++ test_LittleLemon/test_LittleLemon/urls.py
@@ -15,8 +15,38 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from restaurant import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+#from .views import menuView
+#from .views import bookingView 
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+#urlpatterns = [
+ #   path('admin/', admin.site.urls),
+  #  path('restaurant/', include('restaurant.urls')),
+   # #path('menu/', menuView.as_view()),
+    ##path('booking/', bookingview.as_view()),
+    #path('', include(router.urls)),
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+  #  path('api/', include('restaurant.urls')),
+   # path('restaurant/menu/',include('restaurant.urls')),
+ #   path('restaurant/booking/', include(router.urls)),
+#]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/',include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
+    #path('auth/', include('djoser.urls')),
+    #path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
+    #path('api/', include('restaurant.urls')),
+
 ]
+
+#router = routers.DefaultRouter()
+#router.register(r'user', views.UserViewSet)
