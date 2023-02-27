8014c7e9c61f57de3ef19076fd00bcc07d3dfbaf
Susan <susanjoaguilar@gmail.com>
2023-02-26 22:48:37 -0600

    setting up the models

--- test_LittleLemon/restaurant/models.py
+++ test_LittleLemon/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key= True)
+    name = models.CharField(max_length=255)
+    numberOfGuest = models.IntegerField()
+    bookingDate = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def __str__(self)-> str:
+        return str(self.title) + ' : ' + str(self.price)
+    
